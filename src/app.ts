9f729317d3a532eda9d6839c635923a836860ef0
LuizFelipeOC <luizfelipeeoliveiraac@gmail.com>
2026-02-10 21:59:15 -0300

    feat: add Zod type provider to Fastify app initialization

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import './config/env'
 import fastify from "fastify";
 import { registerRoutes } from "./routes";
+import { ZodTypeProvider } from 'fastify-type-provider-zod';
 
 export const app = fastify({
     logger: true,
-});
+}).withTypeProvider<ZodTypeProvider>();
 
 registerRoutes(app);
+
 
